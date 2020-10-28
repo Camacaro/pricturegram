d7721546822e455c4dc5878bc3e190512ae8b7b5
Jesus Camacaro <jesus.camacaro@imageimaker.com>
2020-10-28 15:34:26 -0300

    postgreSQL

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -81,10 +81,20 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'picturegram',
+        'USER': 'postgres',
+        'PASSWORD': 'dams123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
